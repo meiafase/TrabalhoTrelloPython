0b00b36cd13526c6e804be8a562acbffb8b4d687
samuel diel <samuelsdiel@gmail.com>
2024-09-10 11:09:53 -0300

    cadastro de usuario finalizado

--- Trello/mysite/cadastro/views.py
+++ Trello/mysite/cadastro/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect, render, redirect
-
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from board.models import Usuario
 
 def cadastro(request):
@@ -7,16 +8,25 @@ def cadastro(request):
         usuario = request.POST.get('usuario')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmarSenha')
+
+        print(usuario, email, senha, confirmar_senha)
 
         if Usuario.objects.filter(nome=usuario).exists():
-            return render(request, 'cadastro/cadastro.html')
+            messages.error(request, 'O nome de usuário já está em uso.')
+            return render(request, 'cadastro/IndexCadastro.html')
 
         if Usuario.objects.filter(email=email).exists():
+            messages.error(request, 'O e-mail já está em uso.')
             return render(request, 'cadastro/IndexCadastro.html')
-        
-        Usuario.objects.create_user(username=usuario, email=email, password=senha)
-        return redirect('login')  
 
-        
+        if senha != confirmar_senha:
+            messages.error(request, 'As senhas não coincidem.')
+            return render(request, 'cadastro/IndexCadastro.html')
+
+        Usuario.objects.create(nome=usuario, email=email, senha=senha)
+        messages.success(request, 'Cadastro realizado com sucesso! Faça login para continuar.')
+        return redirect("../")
+
     else:
-        return render(request, 'cadastro/cadastro.html')
+        return render(request, 'cadastro/IndexCadastro.html')
